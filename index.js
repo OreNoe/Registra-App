09a12a2910e2b393bd4de3bce825b3add566e6bf
Oren Noe <orennoe@gmail.com>
2023-03-01 18:41:39 -0300

    Nice

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const {google} = require('googleapis');
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = require(GOOGLE_APPLICATION_CREDENTIALS);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -11,7 +11,7 @@ const db = admin.firestore();
 
 
 const auth = new google.auth.GoogleAuth({
-    keyFile: './serviceAccountKey.json',
+    keyFile: GOOGLE_APPLICATION_CREDENTIALS.keyFile,
     scopes: 'https://www.googleapis.com/auth/spreadsheets',
 });
 
